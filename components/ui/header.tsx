40e99f565b5e677200ef7091920480cd5eb86020
Ch3tanUppin <sathyprakashuppin@gmail.com>
2024-10-22 20:15:58 +0530

    first commit

---
+++ components/ui/header.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+import Logo from "./logo";
+
+export default function Header() {
+  return (
+    <header className="z-30 mt-2 w-full md:mt-5">
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+      </div>
+    </header>
+  );
+}
